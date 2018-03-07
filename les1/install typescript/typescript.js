bedb9f37a454313d9140222cef100403e7004527
24877 <36764276+24877@users.noreply.github.com>
2018-03-07 09:27:50 +0100

    Add files via upload

---
+++ les1/install typescript/typescript.js
@@ -0,0 +1,6 @@
+//les 1, opdracht: installeer typescript(gold - achievement)
+function HELLO(person) {
+    return "hello " + person;
+}
+var user = "Jarno";
+console.log(HELLO(user));
---
+++ les1/install typescript/typescript.ts
@@ -0,0 +1,9 @@
+//les 1, opdracht: installeer typescript(gold - achievement)
+
+function HELLO(person) {
+    return "hello " + person;
+}
+
+var user = "Jarno";
+
+console.log( HELLO(user) );
